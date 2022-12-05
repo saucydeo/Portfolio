eadaaee9d80397c38581ba850c4037cd3b6d9ea6
saucydeo <107665031+saucydeo@users.noreply.github.com>
2022-12-05 17:35:33 -0500

    Update Dating_Application.cpp

--- Dating Application/Dating_Application.cpp
+++ Dating Application/Dating_Application.cpp
@@ -3,40 +3,53 @@
 using namespace std;
 class user1{
     public:
-        int age, height;
-        string sex, firstName, lastName,
-        sexual_prefrence, intrests;
+        int age_, height_;
+        string sex_, first_Name_, last_Name_,
+        sexual_prefrence_, intrests_;
 
+    public:
+       void print_Details(){
+            cout<<"\\\\\\\\\\\\\\\\\\\\"<<first_Name_<<"'s Dating Information///////////////////\n";
+            cout<<"|Full name is: "<<first_Name_ + last_Name_<<"                         |\n";
+            cout<<"|Your sex is: "<<sex_<<"                                           |\n";
+            cout<<"|Your age is: "<<age_<<"                                           |\n";
+            cout<<"|Your height is: "<<height_<<"                                  |\n";
+            cout<<"|Your partner's sex: "<<sexual_prefrence_<<"                  |\n";
+        }
 };
 
-void gatheringData();
+void gatheringData(string sex, string firstName, string lastName, int age, int height, string sexual_prefrence, string intrests);
+void findPartner();
+void findHeight();
 
 int main()
-{
+{   
+    int age,height;
+    string sex, firstName, lastName, sexual_prefrence, intrests;
+    bool girls = false, boys = false;
     
-    gatheringData();
+    gatheringData(sex,firstName,lastName,age,height,sexual_prefrence,intrests);
+    cout<<sex;
 
     return 0;
 }
 
-void gatheringData(){
-    int age,height;
-    string sex, firstName, lastName, sexual_prefrence, intrests;
+void gatheringData(string sex, string firstName, string lastName, int age, int height, string sexual_prefrence, string intrests){
 
     cout<<"\\\\\\\\\\\\\\\\\\\\Dene's Dating Application///////////////////\n";
     cout<<"|                Welcome to Love at first glance!    |\n";
     cout<<"|What is your sex(Male/Female): ";
-    cin>>sex;
+    getline(cin>>ws,sex);
     cout<<"|What is your first name: ";
-    cin>>firstName;
+    getline(cin>>ws,firstName);
     cout<<"|What is your last name: "; 
-    cin>>lastName;
+    getline(cin>>ws,lastName);
     cout<<"|How old are you: ";
     cin>>age;
     cout<<"|How tall are you(inches): ";
     cin>>height;
     cout<<"|What is your sexual prefrence: ";
-    cin>>sexual_prefrence;
+    getline(cin>>ws,sexual_prefrence);
     cout<<"|What is your hobby or intrests: ";
     getline(cin>>ws,intrests);
 }
