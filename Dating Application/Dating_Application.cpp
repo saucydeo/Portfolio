b1ccb77a561c6223092446fa8dc92cf84ad4e514
saucydeo <107665031+saucydeo@users.noreply.github.com>
2022-12-07 22:23:37 -0500

    Update Dating_Application.cpp

--- Dating Application/Dating_Application.cpp
+++ Dating Application/Dating_Application.cpp
@@ -4,25 +4,97 @@ using namespace std;
 class user1{
     public:
         int age_;
+        string change;
         char feet_, inches_;
         string sex_, first_Name_, last_Name_,
-        sexual_prefrence_, intrests_;
+        sexual_prefrence_, intrests_, choice;
 
     public:
+        void Change(){
+            string name1,name2,choice2,choice5,choice6;
+            int choice,choice3;
+            double choice4;
+
+            cout<<"1. Name\n";
+            cout<<"2. Sex\n";
+            cout<<"3. Age\n";
+            cout<<"4. Height\n";
+            cout<<"5. Sexual Prefrence\n";
+            cout<<"6. Intrests\n";
+            cout<<"What would you like to change: ";
+            cin>>choice;
+            while(true){
+                switch(choice){
+                    case 1:
+                        cout<<"What is your first name: ";
+                        cin>>name1;
+                        cout<<"What is your last name: ";
+                        cin>>name2;
+                        user1 User;
+    User.age_ = age;
+    User.first_Name_ = firstName;
+    User.last_Name_ = lastName;
+    User.sex_ = sex;
+    User.feet_ = feet;
+    User.inches_ = inches;
+    User.intrests_ = intrests;
+    User.sexual_prefrence_ = sexual_prefrence;
+    User.print_Details();
+                        goto exit_loop;
+                    case 2:
+                        cout<<"What is your sex: ";
+                        cin>>choice2;
+                        goto exit_loop;
+                    case 3:
+                        cout<<"What is your age: ";
+                        cin>>choice3;
+                        goto exit_loop;
+                    case 4:
+                        cout<<"What is your height(inches): ";
+                        cin>>choice4;
+                        goto exit_loop;
+                    case 5:
+                        cout<<"What is your sexual prefrence: ";
+                        cin>>choice5;
+                        goto exit_loop;
+                    case 6:
+                        cout<<"What is your intrests: ";
+                        cin>>choice6;
+                        goto exit_loop;
+                    default:
+                        cout<<"Please enter a valid number.\n";
+                        cout<<"What would you like to change: ";
+                        cin>>choice;
+                        goto exit_loop;
+                    }
+                }
+                exit_loop:;
+                user1 User;
+                User.age_ = choice3;
+                User.first_Name_ = name1;
+                User.last_Name_ = name2;
+                User.sex_ = choice2;
+                User.intrests_ = choice6;
+                User.sexual_prefrence_ = choice5;
+                User.print_Details();
+
+}
        void print_Details(){
             cout<<"\\\\\\\\\\\\\\\\\\\\"<<first_Name_<<"'s Dating Information///////////////////\n";
-            cout<<"|Full name is: "<<first_Name_ +" "+last_Name_<<"                         |\n";
-            cout<<"|Your sex is: "<<sex_<<"                                           |\n";
-            cout<<"|Your age is: "<<age_<<"                                           |\n";
-            cout<<"|Your height is: "<<feet_<<"'"<<inches_<<"                                  |\n";
-            cout<<"|Your partner's sex: "<<sexual_prefrence_<<"                  |\n";
+            cout<<"|Full name is: "<<first_Name_ +" "+last_Name_<<"                          |\n";
+            cout<<"|Your sex is: "<<sex_<<"                                     |\n";
+            cout<<"|Your age is: "<<age_<<"                                    |\n";
+            cout<<"|Your height is: "<<feet_<<"'"<<inches_<<"                                 |\n";
+            cout<<"|Your partner's sex: "<<sexual_prefrence_<<"                            |\n";
+            cout<<"|Intrests: "<<intrests_<<"              \n";
         }
 };
 double findHeight(double height){
     double total = height/12;
     return total;
+
 }
-void gatheringData(string sex,string firstName, string lastName, int age, double height, string sexual_prefrence, string intrests){
+void gatheringData(string sex,string firstName, string lastName, int age, double height, string sexual_prefrence, string intrests, string change){
     cout<<"\\\\\\\\\\\\\\\\\\\\Dene's Dating Application///////////////////\n";
     cout<<"|                Welcome to Love at first glance!    |\n";
     cout<<"|What is your sex(Male/Female): ";
@@ -55,6 +127,11 @@ void gatheringData(string sex,string firstName, string lastName, int age, double
     User.intrests_ = intrests;
     User.sexual_prefrence_ = sexual_prefrence;
     User.print_Details();
+    cout<<"|Would you like to change any of your information: ";
+    cin>>change;
+    if(change=="yes"||change=="YES"||change=="Y"||change=="y"){
+        Change();
+    }
 
 }
 void findPartner();
@@ -63,10 +140,10 @@ int main()
 {   
     int age;
     int height;
-    string sex, firstName, lastName, sexual_prefrence, intrests;
+    string sex, firstName, lastName, sexual_prefrence, intrests,change;
     bool girls = false, boys = false;
     
-    gatheringData(sex,firstName,lastName,age,height,sexual_prefrence,intrests);
+    gatheringData(sex,firstName,lastName,age,height,sexual_prefrence,intrests,change);
 
     return 0;
 }
