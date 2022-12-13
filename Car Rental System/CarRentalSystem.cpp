aea17728d5a51824893d011db14fac775b7a154e
saucydeo <107665031+saucydeo@users.noreply.github.com>
2022-12-13 16:25:22 -0500

    Update CarRentalSystem.cpp

--- Car Rental System/CarRentalSystem.cpp
+++ Car Rental System/CarRentalSystem.cpp
@@ -1,272 +1,159 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 using namespace std;
-
-void Car_Rental(string USERNAME);
+void toUpper(std::string &str) {
+    if (str.length() == 0) {
+        return;
+    }
  
-class Toyota{
+    str[0] = std::toupper(str[0]);
+}
+class user1{
     public:
-        string name;
-        string model;
-        int price;
-        int car_no;
-        int number_days;
-        int cleaning_fee;
-        int damages;
-    
-    public:
-        void printRecipet(int days){
-            cout<<"              Car Rental Invoice          \n";
-            cout<<"/////////////////////////////////////////////\n";
-            cout<<"|Name: "<<name<<"                         |\n";
-            cout<<"|Model: "<<model<<"                                |\n";
-            cout<<"|Number of days: "<<days<<"                          |\n";
-            cout<<"|Price: "<<price*days<<"                                 |\n";
-            cout<<"|Car Number: "<<car_no<<"                           |\n";
-            cout<<"|Cleaning fee: "<<cleaning_fee<<"                           |\n";
-            cout<<"|Car damages: "<<damages<<"                             |\n";
-            cout<<"|Total: "<<price*days+cleaning_fee+damages<<"                                 |\n";
-        }
-
-};
-class Mercedes{
-    public:
-        string name;
-        string model;
-        int price;
-        int car_no;
-        int number_days;
-        int cleaning_fee;
-        int damages;
+        int age_;
+        char feet_, inches_;
+        string sex_, first_Name_, last_Name_,
+        sexual_prefrence_, intrests_, choice;
 
     public:
-        void printRecipet(int days){
-            cout<<"              Car Rental Invoice          \n";
-            cout<<"/////////////////////////////////////////////\n";
-            cout<<"|Name: "<<name<<"                        |\n";
-            cout<<"|Model: "<<model<<"                             |\n";
-            cout<<"|Number of days: "<<days<<"                          |\n";
-            cout<<"|Price: "<<price*days<<"                                |\n";
-            cout<<"|Car Number: "<<car_no<<"                           |\n";
-            cout<<"|Cleaning fee: "<<cleaning_fee<<"                           |\n";
-            cout<<"|Car damages: "<<damages<<"                             |\n";
-            cout<<"|Total: "<<price*days+cleaning_fee+damages<<"                                |\n";
-        }
-
-
-};
-class Mercedes_Gle{
-    public:
-        string name;
-        string model;
-        int price;
-        int car_no;
-        int number_days;
-        int cleaning_fee;
-        int damages;
-
-    public:
-        void printRecipet(int days){
-            cout<<"              Car Rental Invoice          \n";
-            cout<<"/////////////////////////////////////////////\n";
-            cout<<"|Name: "<<name<<"                        |\n";
-            cout<<"|Model: "<<model<<"                           |\n";
-            cout<<"|Number of days: "<<days<<"                          |\n";
-            cout<<"|Price: "<<price*days<<"                                |\n";
-            cout<<"|Car Number: "<<car_no<<"                           |\n";
-            cout<<"|Cleaning fee: "<<cleaning_fee<<"                           |\n";
-            cout<<"|Car damages: "<<damages<<"                             |\n";
-            cout<<"|Total: "<<price*days+cleaning_fee+damages<<"                                |\n";
-        }
-
-};
-class BMW{
-    public:
-        string name;
-        string model;
-        int price;
-        int car_no;
-        int number_days;
-        int cleaning_fee;
-        int damages;
-
-    public:
-        void printRecipet(int days){
-            cout<<"              Car Rental Invoice          \n";
-            cout<<"/////////////////////////////////////////////\n";
-            cout<<"|Name: "<<name<<"                             |\n";
-            cout<<"|Model: "<<model<<"                                  |\n";
-            cout<<"|Number of days: "<<days<<"                          |\n";
-            cout<<"|Price: "<<price*days<<"                                |\n";
-            cout<<"|Car Number: "<<car_no<<"                           |\n";
-            cout<<"|Cleaning fee: "<<cleaning_fee<<"                           |\n";
-            cout<<"|Car damages: "<<damages<<"                             |\n";
-            cout<<"|Total: "<<price*days+cleaning_fee+damages<<"                                |\n";
+       void print_Details(){
+            toUpper(first_Name_);
+            toUpper(last_Name_);
+            toUpper(sex_);
+            toUpper(sexual_prefrence_);
+            toUpper(intrests_);
+            cout<<"\\\\\\\\\\\\\\\\\\\\"<<first_Name_<<"'s Dating Information///////////////////\n";
+            cout<<"|Full name is: "<<first_Name_+" "+last_Name_<<"\n";
+            cout<<"|Your sex is: "<<sex_<<"\n";
+            cout<<"|Your age is: "<<age_<<"\n";
+            cout<<"|Your height is: "<<feet_<<"'"<<inches_<<"\n";
+            cout<<"|Your partner's sex: "<<sexual_prefrence_<<"\n";
+            cout<<"|Intrests: "<<intrests_<<"\n";
         }
 };
-class Honda{
-    public:
-        string name;
-        string model;
-        int price;
-        int car_no;
-        int number_days;
-        int cleaning_fee;
-        int damages;
+double findHeight(double height){
+    double total = height/12;
+    return total;
+}
+void gatheringData(string sex,string firstName, string lastName, int age, double height, string sexual_prefrence, string intrests, string change){
+    string choice,name1,name2,choice2,choice5,choice6;
+    int choice3;
+    double choice4;
 
-    public:
-        void printRecipet(int days){
-            cout<<"              Car Rental Invoice          \n";
-            cout<<"/////////////////////////////////////////////\n";
-            cout<<"|Name: "<<name<<"                          |\n";
-            cout<<"|Model: "<<model<<"                                |\n";
-            cout<<"|Number of days: "<<days<<"                          |\n";
-            cout<<"|Price: "<<price*days<<"                                 |\n";
-            cout<<"|Car Number: "<<car_no<<"                           |\n";
-            cout<<"|Cleaning fee: "<<cleaning_fee<<"                           |\n";
-            cout<<"|Car damages: "<<damages<<"                             |\n";
-            cout<<"|Total: "<<price*days+cleaning_fee+damages<<"                                 |\n";
+    cout<<"\\\\\\\\\\\\\\\\\\\\Dene's Dating Application///////////////////\n";
+    cout<<"|                Welcome to Love at first glance!    |\n";
+    cout<<"|What is your sex(Male/Female): ";
+    getline(cin>>ws,sex);
+    while(true){
+        if(sex!="male"||sex!="Male"||sex!="MALE"||sex!="female"||sex!="Female"||sex!="FEMALE"){
+            cout<<"|What is your sex(Male/Female): ";
+            getline(cin>>ws,sex);
         }
-};
+        else{
+            break;
+        }
+    }
+    cout<<"|What is your first name: ";
+    getline(cin>>ws,firstName);
+    cout<<"|What is your last name: "; 
+    getline(cin>>ws,lastName);
+    cout<<"|How old are you: ";
+    cin>>age;
+    cout<<"|How tall are you(inches): ";
+    cin>>height;
+    cout<<"|What is your sexual prefrence(Male/Female): ";
+    getline(cin>>ws,sexual_prefrence);
+    cout<<"|What is your hobby or intrests: ";
+    getline(cin>>ws,intrests);
 
+    double true_height = findHeight(height);
+    string height_true = to_string(true_height);
+    char feet = height_true[0];
+    char inches = height_true[2];
 
+    user1 User;
+    User.age_ = age;
+    User.first_Name_ = firstName;
+    User.last_Name_ = lastName;
+    User.sex_ = sex;
+    User.feet_ = feet;
+    User.inches_ = inches;
+    User.intrests_ = intrests;
+    User.sexual_prefrence_ = sexual_prefrence;
+    User.print_Details();
+    while(true){
+        cout<<"|Would you like to change any of your information: ";
+        cin>>change;
+        if(change=="yes"||change=="YES"||change=="Y"||change=="y"){
+                cout<<"1. Name\n";
+                cout<<"2. Sex(Male/Female)\n";
+                cout<<"3. Age\n";
+                cout<<"4. Height\n";
+                cout<<"5. Sexual Prefrence(Male/Female)\n";
+                cout<<"6. Intrests\n";
+                cout<<"What would you like to change: ";
+                cin>>choice;
+                if(choice=="1"||choice=="Name"||choice=="name"){
+                    cout<<"What is your first name: ";
+                    cin>>name1;
+                    cout<<"What is your last name: ";
+                    cin>>name2;
+                    User.first_Name_ = name1;
+                    User.last_Name_ = name2;
+                    User.print_Details();
+                }
+                else if(choice=="2"||choice=="sex"||choice=="Sex"){
+                    cout<<"What is your sex: ";
+                    cin>>choice2;
+                    User.sex_ = choice2;
+                    User.print_Details();
+                }
+                else if(choice=="3"||choice=="age"||choice=="Age"){
+                    cout<<"What is your age: ";
+                    cin>>choice3;
+                    User.age_ = choice3;
+                    User.print_Details();
+                }
+                else if(choice=="4"||choice=="Height"||choice=="height"){
+                    cout<<"What is your height: ";
+                    cin>>choice4;
+                    double true_height = findHeight(choice4);
+                    string height_true = to_string(true_height);
+                    char feet_new = height_true[0];
+                    char inches_new = height_true[2];
+                    User.feet_ = feet_new;
+                    User.inches_ = inches_new;
+                    User.print_Details();
+                }
+                else if(choice=="5"||choice=="sexual prefrence"||choice=="Sexual prefrence"||choice=="Sexual Prefrence"){
+                    cout<<"What is your sexual prefrence: ";
+                    cin>>choice5;
+                    User.sexual_prefrence_ = choice5;
+                    User.print_Details();
+                }
+                else if(choice=="6"||choice=="intrests"||choice=="Intrests"){
+                    cout<<"What is your intrests: ";
+                    cin>>choice6;
+                    User.intrests_ = choice6;
+                    User.print_Details();
+                }
+        } 
+        else if(change=="no"||change=="No"||change=="NO"||change=="N"||change=="n"){
+            break;
+        }
+    }
+}
+void findPartner();
+void generatePartners();
 int main()
 {   
-    string USERNAME, PASSWORD;
-    string accounts[] = {"admin", "john", "root","kevianna"};
-    string passwords[] = {"admin", "appleseed", "root","simpson"};
-
-    cout<<"------------------------------\n";
-    cout<<"            LOGIN           \n";
-    cout<<"------------------------------\n";
-
-    while(1){
-    cout<<"Username: ";
-    getline(cin >> ws, USERNAME);
-    if(USERNAME=="exit"||USERNAME=="EXIT"){
-        cout<<"Thank you for using Car Rental Services!";
-        break;
-    }
-    cout<<"Password: ";
-    getline(cin >> ws, PASSWORD);
-    if(PASSWORD=="exit"||PASSWORD=="EXIT"){
-        cout<<"Thank you for using Car Rental Services!";
-        break;
-    }
-
-    if(USERNAME == accounts[0] && PASSWORD == passwords[0])
-    {
-        cout<<"Thank you for logging in "<<USERNAME<<"!\n";
-        Car_Rental(USERNAME);
-        break;
-    }
-    else if(USERNAME == accounts[1] && PASSWORD == passwords[1])
-    {
-        cout<<"Thank you for logging in "<<USERNAME<<"!\n";
-        Car_Rental(USERNAME);
-        break;
-    }
-    else if(USERNAME == accounts[2] && PASSWORD == passwords[2])
-    {
-        cout<<"Thank you for logging in "<<USERNAME<<"!\n";
-        Car_Rental(USERNAME);
-        break;
-    }
-    else if(USERNAME == accounts[3] && PASSWORD == passwords[3])
-    {
-        cout<<"Thank you for logging in "<<USERNAME<<"!\n";
-        Car_Rental(USERNAME);
-        break;
-    }
-    }
+    int age;
+    int height;
+    string sex, firstName, lastName, sexual_prefrence, intrests,change;
+    bool girls = false, boys = false;
+    
+    gatheringData(sex,firstName,lastName,age,height,sexual_prefrence,intrests,change);
 
     return 0;
-}
-void Car_Rental(string USERNAME)
-{
-    int days,car;
-    Toyota car1;
-    car1.name = "Toyota Camry";
-    car1.model = "2022";
-    car1.price = 155;
-    car1.car_no = 5678;
-    car1.number_days = days;
-    car1.cleaning_fee = 25;
-    car1.damages = 0;
-
-    Mercedes car2;
-    car2.name = "Mercedes-Benz";
-    car2.model = "C-Class";
-    car2.price = 185;
-    car2.car_no = 7891;
-    car2.number_days = days;
-    car2.cleaning_fee = 55;
-    car2.damages = 0;
-
-    Mercedes_Gle car3;
-    car3.name = "Mercedes-Benz";
-    car3.model = "GLE Coupe";
-    car3.price = 225;
-    car3.car_no = 3459;
-    car3.number_days = days;
-    car3.cleaning_fee = 90;
-    car3.damages = 0;
-
-    BMW car4;
-    car4.name = "2023 BMW";
-    car4.model = "M3";
-    car4.price = 255;
-    car4.car_no = 8342;
-    car4.number_days = days;
-    car4.cleaning_fee = 95;
-    car4.damages = 0;
-
-    Honda car5;
-    car5.name = "Honda Civic";
-    car5.model = "2023";
-    car5.price = 160;
-    car5.car_no = 4329;
-    car5.number_days = days;
-    car5.cleaning_fee = 65;
-    car5.damages = 0;
-
-    cout<<"            Car Rental           \n";
-    cout<<"////////////////////////////////////\n";
-    cout<<"1. 2022 Toyota Camry\n"<<"2. Mercedes-Benz C-Class\n"<<"3. Mercedes-Benz GLE Coupe\n"<<"4. 2023 BMW M3\n"<<"5. 2023 Honda Civic\n";
-    cout<<"Car Rental Engine: Pick an option, "<<USERNAME<<": ";
-    cin>>car;
-
-    while(true){
-        switch(car){
-        case 1:
-            cout<<"Car Rental Engihe: How long will you rent this car: ";
-            cin >> days;
-            car1.printRecipet(days);
-            goto exit_loop;
-        case 2:
-            cout<<"Car Rental Engihe: How long will you rent this car: ";
-            cin >> days;
-            car2.printRecipet(days);
-            goto exit_loop;
-        case 3:
-            cout<<"Car Rental Engihe: How long will you rent this car: ";
-            cin >> days;
-            car3.printRecipet(days);
-            goto exit_loop;
-        case 4:
-            cout<<"Car Rental Engihe: How long will you rent this car: ";
-            cin >> days;
-            car4.printRecipet(days);
-            goto exit_loop;
-        case 5:
-            cout<<"Car Rental Engihe: How long will you rent this car: ";
-            cin >> days;
-            car5.printRecipet(days);
-            goto exit_loop;
-        default:
-            cout<<"Car Rental Engine: Pick an option, "<<USERNAME<<": ";
-            cin>>car;
-    }
-    }
-    exit_loop: ;
 }
